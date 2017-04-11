bc57c76799f94cedb01d9e89036f68efe13a8c7b
Zhanlong Yang <673029686@qq.com>
2017-04-11 22:57:30 +0800

    Refine codes

--- app/src/main/java/com/example/user/myapps/MainActivity.java
+++ app/src/main/java/com/example/user/myapps/MainActivity.java
@@ -3,63 +3,36 @@ package com.example.user.myapps;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button button_1, button_2, button_3, button_4, button_5, button_6;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void button_1(View view) {
+        Toast.makeText(MainActivity.this, "This is SPOTIFY STREAMER.", Toast.LENGTH_SHORT).show();
+    }
 
-        button_1 = (Button) findViewById(R.id.button_1);
-        button_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This is SPOTIFY STREAMER.", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void button_2(View view) {
+        Toast.makeText(MainActivity.this, "This is SCORES APP.", Toast.LENGTH_SHORT).show();
+    }
 
-        button_2 = (Button) findViewById(R.id.button_2);
-        button_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This is SCORES APP.", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void button_3(View view) {
+        Toast.makeText(MainActivity.this, "This is LIBRARY APP.", Toast.LENGTH_SHORT).show();
+    }
 
-        button_3 = (Button) findViewById(R.id.button_3);
-        button_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This is LIBRARY APP.", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void button_4(View view) {
+        Toast.makeText(MainActivity.this, "This is BUILD IT BIGGER.", Toast.LENGTH_SHORT).show();
+    }
 
-        button_4 = (Button) findViewById(R.id.button_4);
-        button_4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This is BUILD IT BIGGER.", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void button_5(View view) {
+        Toast.makeText(MainActivity.this, "This is XYZ READER.", Toast.LENGTH_SHORT).show();
+    }
 
-        button_5 = (Button) findViewById(R.id.button_5);
-        button_5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This is XYZ READER.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button_6 = (Button) findViewById(R.id.button_6);
-        button_6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This is CAPSTONE:MY OWN APP.", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void button_6(View view) {
+        Toast.makeText(MainActivity.this, "This is CAPSTONE:MY OWN APP.", Toast.LENGTH_SHORT).show();
     }
 }
